47f300b9f136de31c3a9d6a6f1072efac7e1fb08
Keitaro-Hasegawa <keitaro.hasegawa@mixi.co.jp>
2017-08-08 13:25:43 +0900

    add file

---
+++ Sample.cpp
@@ -0,0 +1,12 @@
+//-------------------------
+// 基本文字コードはsjis
+// 改行コードはCRLF
+//
+//-------------------------
+#include <stdio.h>
+
+int main ( ) {
+    printf( "hello!\n" );
+    getchar();
+    return 0;
+}
